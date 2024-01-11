ac1d767794544b38bd4dce36cffa0dfbb29c5014
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-01-11 18:42:22 +0100

    mourir

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {VehicleListComponent} from "./vehicle-list/vehicle-list.component";
+import {CitiesComponent} from "./cities/cities.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   imports: [
-    VehicleListComponent
+    VehicleListComponent,
+    CitiesComponent
   ],
   styleUrls: ['./app.component.css']
 })
---
+++ site/src/app/cities/cities.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {AutoCompleteModule} from "primeng/autocomplete";
+
+interface AutoCompleteCompleteEvent {
+  originalEvent: Event;
+  query: string;
+}
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.component.html',
+  styleUrl: './cities.component.css',
+  imports: [
+    ReactiveFormsModule,
+    AutoCompleteModule,
+  ],
+  standalone: true
+})
+export class CitiesComponent {
+  items: any[] | undefined;
+
+  selectedItem: any;
+
+  suggestions: any[] = ["france", "brest"];
+
+  search(event: AutoCompleteCompleteEvent) {
+    this.suggestions = ["france", "brest"];
+  }
+}
