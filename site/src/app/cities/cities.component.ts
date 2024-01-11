37bb076829d2b6f22e021bf9cb6961973d8bbf14
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2024-01-11 21:41:24 +0100

    V1 filtre

--- site/src/app/cities/cities.component.ts
+++ site/src/app/cities/cities.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AutoCompleteModule} from "primeng/autocomplete";
 import {trigger} from "@angular/animations";
+import * as xml2js from 'xml2js';
+import * as fs from "fs";
 
 interface AutoCompleteCompleteEvent {
   originalEvent: Event;
@@ -14,6 +16,7 @@ interface AutoCompleteCompleteEvent {
   imports: [
     ReactiveFormsModule,
     AutoCompleteModule,
+    FormsModule,
   ],
   animations : [
     trigger('overlayContentAnimation', [
@@ -22,14 +25,48 @@ interface AutoCompleteCompleteEvent {
   ],
   standalone: true
 })
-export class CitiesComponent {
-  items: any[] | undefined;
+export class CitiesComponent implements OnInit{
+  cities: any;
+  suggestionsStart: any;
+  suggestionsEnd: any;
+  selectedCityStart: any;
+  selectedCityEnd: any;
 
-  selectedItem: any;
+  ngOnInit() {
+    this.cities = [
+      "France",
+      "Germany",
+      "Spain",
+      "United Kingdom"
+    ]
 
-  suggestions: any[] = ["france", "brest"];
+  }
 
-  search(event: AutoCompleteCompleteEvent) {
-    this.suggestions = ["france", "brest"];
+  filterCountryStart(event: AutoCompleteCompleteEvent) {
+    let filtered: any[] = [];
+    let query = event.query;
+
+    for (let i = 0; i < (this.cities as any[]).length; i++) {
+      let country = (this.cities as any[])[i];
+      if (country.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(country);
+      }
+    }
+    console.log(filtered);
+    this.suggestionsStart = filtered;
+  }
+
+  filterCountryEnd(event: AutoCompleteCompleteEvent) {
+    let filtered: any[] = [];
+    let query = event.query;
+
+    for (let i = 0; i < (this.cities as any[]).length; i++) {
+      let country = (this.cities as any[])[i];
+      if (country.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(country);
+      }
+    }
+    console.log(filtered);
+    this.suggestionsEnd = filtered;
   }
 }
